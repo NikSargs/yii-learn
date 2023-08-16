06a74f290adf5377b68804457718199e958a9a01
NikSargs <nik.bishkek00@gmail.com>
2023-08-17 00:58:59 +0600

    update

---
+++ portfolio/views/site/PrivacyPolicy.php
@@ -0,0 +1,18 @@
+<?php
+use yii\helpers\Html;
+
+$this->title = 'Privacy Policy';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="site-privacy-policy">
+  <h1><?= Html::encode($this->title); ?></h1>
+  
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu felis urna. Phasellus nec vehicula nisl. Quisque malesuada diam sed sem scelerisque, sit amet blandit eros mollis. Suspendisse tincidunt convallis dolor, non pharetra lorem. In ut ex volutpat, scelerisque nisl ut, egestas eros. Maecenas placerat mattis enim, et convallis felis posuere sed. Integer mollis ante vitae enim iaculis fermentum. Vestibulum pulvinar efficitur ullamcorper. In placerat enim ac sem dapibus pretium. Morbi in venenatis nibh. Sed porttitor tellus ullamcorper erat tempor convallis. Praesent sit amet massa vel turpis congue sodales eget sed odio. Morbi quis sem eget ante sodales maximus non in odio. Fusce rhoncus, leo vitae pharetra euismod, dolor enim pretium est, et dignissim tortor felis in metus.
+
+  </p>
+  <p>Nullam et porta arcu. Praesent ut ultricies justo. Aliquam lectus tortor, viverra sed blandit ac, tempus eget elit. Mauris vel erat id nunc commodo hendrerit vitae a ex. Aenean vitae cursus ante. Cras a magna nec justo aliquam vestibulum. In facilisis porttitor risus, in placerat nunc tincidunt sit amet. Praesent maximus et nulla a mattis. Nullam dictum viverra viverra. Vivamus id auctor nulla. Nam eleifend tortor in massa fringilla, non mollis lacus facilisis. Nunc at consequat nulla. Vestibulum consequat eros in augue euismod, a lobortis turpis finibus. Etiam mattis feugiat mattis. Duis vel nunc ornare, accumsan lorem eget, fringilla quam.
+    
+  </p>
+
+</div>
